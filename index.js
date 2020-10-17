5da3027e541c504f62103360d5b3167b5aa96b7f
Chanson9892 <chan.hanson5@gmail.com>
2020-10-17 13:36:44 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let w = array.find(s => s.result === "W")
+  if (w !== undefined){
+    return w["year"]
+  } else {
+    return undefined
+  }
+}
