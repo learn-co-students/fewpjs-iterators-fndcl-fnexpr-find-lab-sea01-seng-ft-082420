614f503ab8221937fc7d09752ef3a8fef2c78bd1
jonelle-noelani <jonelle.noelani@gmail.com>
2020-10-25 21:31:50 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record){
+  let result = record.find(record => record.result === "W")
+  return !!result ? result.year : undefined
+}
+// const result = W
+
+// function superbowlWin(arr){
+//   arr.result === "W";
+//   return year
+// }
+// arr.find(superbowlWin){
+//   return year
+// }
+// record.find(superbowlWin(result){
+//   return year; result === "W";
+  
+// })
